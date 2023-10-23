573f9d557d36e2a3432e5e15beb127283f150542
Daniil Vinogradov <daniilvinogradov@cc.com.mt>
2023-10-23 15:37:33 +0200

    Initial Commit

---
+++ LibTorrent/LibTorrent.h
@@ -0,0 +1,18 @@
+//
+//  LibTorrent.h
+//  LibTorrent
+//
+//  Created by Daniil Vinogradov on 23/10/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LibTorrent.
+FOUNDATION_EXPORT double LibTorrentVersionNumber;
+
+//! Project version string for LibTorrent.
+FOUNDATION_EXPORT const unsigned char LibTorrentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibTorrent/PublicHeader.h>
+
+
